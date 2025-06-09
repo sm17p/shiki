510507694a6988c97469ab89645650f1821c103f
Smit <smitp.contact@gmail.com>
2025-06-09 08:28:25 +0530

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-oxc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "node:path";
 
